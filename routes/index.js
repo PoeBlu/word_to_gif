7c51c966ab5b90c33e9eec6b7c2e9905dacf8bff
shekit <as7790@nyu.edu>
2015-11-23 23:18:24 -0500

    added giphy api, basic search working

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/config');
+
+var giphy = require('giphy-api')(config.key);
 
 /* GET home page. */
 
 router.post('/gifit', function(req,res,next){
 	var query = req.body.query
-	res.send(query)
+
+	giphy.search(query, function(err, resp){
+		res.send(resp.data[0].images["original"]);
+	})
 })
 
 
