2d4a7cfdb7deb081ff3a28a578f3f2b569cbbe53
shekit <as7790@nyu.edu>
2015-11-23 22:43:12 -0500

    post request and response working

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
-	console.log("hello")
+	console.log("hello");
+
+	$("form").on('submit', function(event){
+
+		event.preventDefault();
+		var query = $(".gif-query").val();
+
+		if(query){
+
+			$.ajax({
+				"url":"http://localhost:3000/gifit",
+				"method": "POST",
+				"data":{"query":query}
+			})
+			.done(function(resp){
+				console.log(resp)
+			})
+			.error(function(err){
+				console.log(err)
+			})
+
+		} else {
+			console.log("no text detected")
+		}
+
+	})
 
 })
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.post('/gifit', function(req,res,next){
+	var query = req.body.query
+	res.send(query)
+})
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
