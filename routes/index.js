39009245a38de8b2cb9993d4732b578117efc683
shekit <as7790@nyu.edu>
2015-11-25 14:03:21 -0500

    making flickr calls

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,53 @@ var pngFileStream = require('png-file-stream');
 
 var fs = require('fs')
 
+// var Flickr = require("flickrapi"),
+//     flickrOptions = {
+//     	"api-key":config.flickr
+//     }
+var Flickr = require('node-flickr')
+var flickrKey = {'api_key':config.flickr}
+var flickr = new Flickr(flickrKey);
+
 
 /* GET home page. */
 
 router.post('/gifit', function(req,res,next){
 	var query = req.body.query;
 
+	//split query on spaces
+	var queryTerms = query.split(/\s+/);
+
+	var resultUrls = [];
+
+	var callsFinished = 0;
+
+	//for each term search flickr and append to a url list
+	for(var i in queryTerms){
+
+		searchFlickr(i);
+		
+	}
+
+	function searchFlickr(index) {
+		flickr.get('photos.search', {"tags":queryTerms[index]}, function(err,result){
+			if(err){
+				return console.error(err)
+			}
+			callsFinished++;
+			if (callsFinished == queryTerms.length) {
+				// res.send("done");
+			}
+			console.log(result.photos.photo[0].title)
+		})
+	}
+
+
 	//search images and stitch them together
-	
-	giphy.search(query, function(err, resp){
-		res.send(resp.data[0].images["original"]);
-	})
+	res.send("done")
+	// giphy.search(query, function(err, resp){
+	// 	res.send(resp.data[0].images["original"]);
+	// })
 })
 
 router.get('/giff', function(req,res,next){
