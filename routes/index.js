5507926c2118907208525a9b929f8bd44dcc07b8
shekit <as7790@nyu.edu>
2015-11-25 16:02:28 -0500

    downloading images with unique name

--- routes/index.js
+++ routes/index.js
@@ -18,18 +18,23 @@ var Flickr = require('node-flickr')
 var flickrKey = {'api_key':config.flickr}
 var flickr = new Flickr(flickrKey);
 
+var request = require('request');
+
+var shortid = require('shortid');
 
 /* GET home page. */
 
 router.post('/gifit', function(req,res,next){
 	var query = req.body.query;
 
+	var uniqueFilename = shortid.generate();
+
 	//split query on spaces
 	var queryTerms = query.split(/\s+/);
 
 	var resultUrls = [];
 
-	var callsFinished = 0;
+	var imageCounter = 0;
 
 	//for each term search flickr and append to a url list
 	for(var i in queryTerms){
@@ -39,17 +44,52 @@ router.post('/gifit', function(req,res,next){
 	}
 
 	function searchFlickr(index) {
-		flickr.get('photos.search', {"tags":queryTerms[index]}, function(err,result){
+		flickr.get('photos.search', {
+			"text":queryTerms[index],
+			"page":1,
+			"per_page":1,
+			"sort":"interestingness-desc",
+			"media":"photos"
+		}, function(err,result){
 			if(err){
 				return console.error(err)
 			}
-			callsFinished++;
-			if (callsFinished == queryTerms.length) {
-				// res.send("done");
-			}
-			console.log(result.photos.photo[0].title)
+			// callsFinished++;
+			// if (callsFinished == queryTerms.length) {
+			// 	// res.send("done");
+			// }
+
+			imageCounter++;
+
+			var response = result.photos.photo[0]
+			var url = "https://farm"+response["farm"]+".staticflickr.com/"+response["server"]+"/"+response["id"]+"_"+response["secret"]+".jpg";
+			console.log(url);
+
+			//download the image
+			downloadImage(url, uniqueFilename ,imageCounter)
 		})
 	}
+
+	function downloadImage(imageUrl,filename,counter){
+		// request.head(imageUrl, function(erro,respo,body){
+		// 	request(imageUrl).pipe(fs.createWriteStream("../images"+filename+counter+".png")).on('close', function(){
+		// 		console.log("downloaded image")
+		// 	})
+		// })
+		// request.get(imageUrl, function(erro, respo, body){
+		// 	fs.write(filename+counter+".png", respo.body, function(){
+		// 		console.log("Downloaded: "+imageUrl)
+		// 	})
+		// })
+		request(imageUrl).pipe(fs.createWriteStream(filename+counter+".png")).on('close', function(){
+			console.log("DOwnloaded")
+		})
+		console.log(imageUrl, filename, counter)
+	}
+
+
+
+	//function down
 
 
 	//search images and stitch them together
