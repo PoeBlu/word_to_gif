06369e66b182d60f8b35f498493adae4bc2d72fd
shekit <as7790@nyu.edu>
2015-12-14 01:48:00 -0500

    implemented loading screen and selection of source

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -4,10 +4,18 @@ $(document).ready(function(){
 
 	var searchApi = 'bing';
 
+	var loading = $(".loading")
+	var bing = $("#searchBing");
+	var giphy = $("#searchGiphy");
+	var flickr = $("#searchFlickr")
+
 	$("body").on('click','#searchGiphy', function(event){
 		event.preventDefault();
 		searchGiphy = !searchGiphy;
 		console.log(searchApi, searchGiphy);
+		giphy.addClass('selected');
+		bing.removeClass('selected');
+		flickr.removeClass('selected');
 	})
 
 	$("body").on('click', '#searchFlickr', function(event){
@@ -15,6 +23,9 @@ $(document).ready(function(){
 		searchApi = 'flickr';	
 		searchGiphy = false;
 		console.log(searchApi, searchGiphy);	
+		giphy.removeClass('selected');
+		bing.removeClass('selected');
+		flickr.addClass('selected');
 	})
 
 	$("body").on('click', '#searchBing', function(event){
@@ -22,18 +33,23 @@ $(document).ready(function(){
 		searchApi = 'bing';
 		searchGiphy = false;
 		console.log(searchApi, searchGiphy);
+		giphy.removeClass('selected');
+		bing.addClass('selected');
+		flickr.removeClass('selected');
 	})
 
 	$("form").on('submit', function(event){
 
 		event.preventDefault();
 		var query = $(".gif-query").val();
-		query = query.trim(); //remove whitespace
+		query = query.toLowerCase().trim(); //remove whitespace
 
 		if(!query){
 			console.log("no query");
 			return;
 		}
+
+		loading.show();
 
 		if(!searchGiphy){
 
@@ -45,10 +61,14 @@ $(document).ready(function(){
 			.done(function(resp){
 				console.log("GOT GIF FROM IMGS")
 				//console.log(resp)
+				loading.hide();
 				showGif(resp)
+				
 			})
 			.error(function(err){
 				console.log(err)
+				loading.hide();
+				alert("oops there was an error, try again")
 			})
 
 		} else {
@@ -60,10 +80,13 @@ $(document).ready(function(){
 			.done(function(resp){
 				console.log("GOT GIF FROM GIFS")
 				//console.log(resp)
+				loading.hide();
 				showGif(resp)
 			})
 			.error(function(err){
 				console.log(err)
+				loading.hide();
+				alert("oops there was an error, try again")
 			})
 		}
 
@@ -71,10 +94,12 @@ $(document).ready(function(){
 
 	function showGif(resp){
 		console.log(resp);
+		$(".result").show();
 		$("#newgif").attr('src',resp);
 		$("#newgifurl").attr('href',resp);
 		$("#newgifurl").html(resp)
 		
 	}
+
 
 })
